43b27690bc055c400f2d295961e1f854f6396338
Akayy <techakayy@gmail.com>
2023-07-09 00:08:35 +1000

    Updated link to landing page

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -29,7 +29,7 @@
               size="xl"
               label="Buy Now"
               target="_blank"
-              to="https://github.com/pinegrow/natures-delight-with-iles-tailwindcss"
+              to="https://vuedesigner.com"
             >
             </BaseButton>
           </div>
