50a00dafe6d92812075bcc4f4509e11e86039e37
Akayy <techakayy@gmail.com>
2023-08-19 17:55:22 +1000

    tsconfig updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
   get content() {
     const _content = [
       './index.html',
-      './src/**/*.{html,vue,svelte,astro,js,ts,jsx,tsx,md,mdx}',
+      './src/**/*.{html,vue,svelte,astro,js,cjs,ts,cts,mts,jsx,tsx,md,mdx}',
     ]
     return process.env.NODE_ENV === 'production'
       ? _content
