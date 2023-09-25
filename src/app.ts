0c3a1346645091bdbde28002cc6e6b5216e6d90b
Akayy <techakayy@gmail.com>
2023-09-25 15:29:08 +1000

    Minor edit

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,11 @@
 //   window.devtools = devtools
 // }
 
-import { defineApp } from 'iles'
 import 'uno.css'
 import '@/assets/css/tailwind.css'
 import 'prismjs/themes/prism-tomorrow.css'
 
+import { defineApp } from 'iles'
 import pinia from '@/plugins/pinia'
 
 export default defineApp({
