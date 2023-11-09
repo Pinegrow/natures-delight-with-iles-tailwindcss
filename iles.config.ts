7fbde77a80fdff80432aa98163be5f44e895a631
Akayy <techakayy@gmail.com>
2023-11-09 20:34:16 +1100

    Updated unocss

--- iles.config.ts
+++ iles.config.ts
@@ -142,6 +142,11 @@ export default defineConfig({
             prefix: 'i-', // default prefix, do not change
           }),
         ],
+        content: {
+          pipeline: {
+            include: ['./src/**/*'],
+          },
+        },
       }),
       // VueDevTools(),
     ],
